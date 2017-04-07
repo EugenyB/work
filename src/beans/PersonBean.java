f26594affda7eb8e74c586b4ac95706ae0d10f4c
Vladimir <vladimir.ku.web@gmail.com>
2017-04-07 10:59:14 +0300

    07.04

--- src/beans/PersonBean.java
+++ src/beans/PersonBean.java
@@ -1,14 +1,41 @@
 package beans;
 
 
-import javax.faces.bean.RequestScoped;
+
+import dao.PersonDAO;
+import dtables.Person;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by Vladimir on 12.03.2017.
  */
 @Named
-@RequestScoped
-public class PersonBean {
+@SessionScoped
+public class PersonBean implements Serializable{
+    @EJB
+    PersonDAO personDAO;
 
+    private Person current;
+
+    public Person getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Person current) {
+        this.current = current;
+    }
+
+
+    public List<Person> getPeople() {
+        return personDAO.findAll();
+    }
+
+    public String login() {
+        return "success";
+    }
 }
